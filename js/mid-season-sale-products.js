3453ed72e24df24799a915e01785f7b9dad13452
VHarasymiv <volodymyr.harasymiv@gmail.com>
2021-11-29 13:53:53 +0000

    added styles for asc and desc button in mid-season-sale section

--- js/mid-season-sale-products.js
+++ js/mid-season-sale-products.js
@@ -1,11 +1,5 @@
 (function () {
 
-    const clock = document.querySelector(".clock");
-    function updateClock() {
-        clock.innerText = new Date().toLocaleTimeString();
-    }
-
-    setInterval(updateClock, 1000);
     const products = [
         {
             id: "1",
