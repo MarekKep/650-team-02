3d992e3b4ab08ac2bdf92a80904e0d96b9063904
IrynaDulya <iruna.dulya@gmail.com>
2021-11-30 11:33:44 +0000

    add timer

--- js/timer.js
+++ js/timer.js
@@ -19,10 +19,10 @@ document.addEventListener('DOMContentLoaded', function () {
     $hours.textContent = hours < 10 ? '0' + hours : hours;
     $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
     $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
-    $days.dataset.title = declensionNum(days, ['Day', 'Days']);
-    $hours.dataset.title = declensionNum(hours, ['Hour', 'Hours']);
-    $minutes.dataset.title = declensionNum(minutes, ['Minute','Minutes']);
-    $seconds.dataset.title = declensionNum(seconds, ['Second', 'Seconds']);
+    $days.dataset.title = declensionNum(days, ['Day','Day','Days']);
+    $hours.dataset.title = declensionNum(hours, ['Hour','Hour','Hours']);
+    $minutes.dataset.title = declensionNum(minutes, ['Minute','Minute','Minutes']);
+    $seconds.dataset.title = declensionNum(seconds, ['Second','Second','Seconds']);
   }
   const $days = document.querySelector('.timer__days');
   const $hours = document.querySelector('.timer__hours');
